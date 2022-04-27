4c4d368c3d1d210136cdcf784795359b5584ecc0
seunglo <devseung9056@gmail.com>
2022-04-27 12:30:29 +0900

    트랜잭션 이해 실습 완료

--- src/main/java/hello/jdbc/repository/MemberRepositoryV2.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV2.java
@@ -1,10 +1,9 @@
 package hello.jdbc.repository;
 
-import hello.jdbc.connection.DBConnectionUtil;
+import hello.jdbc.connection.ConnectionConst;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.support.JdbcUtils;
-import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -13,14 +12,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.NoSuchElementException;
 
-/*
-*   Datasource 사용, JdbcUtils 사용
-* */
+/**
+ * 같은 커넥션을 바라보게 하는 레포지토리
+ */
+
 @Slf4j
-public class MemberRepositoryV1 {
+public class MemberRepositoryV2 {
     private DataSource dataSource;
 
-    public MemberRepositoryV1(DataSource dataSource) {
+    public MemberRepositoryV2(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
@@ -75,6 +75,39 @@ public class MemberRepositoryV1 {
         }
     }
 
+    public Member findById(Connection con, String id) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, id);
+
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            }
+            log.info("{}인 맴버는 존재하지 않습니다", id);
+            throw new NoSuchElementException(id + "인 member는 존재하지 않습니다.");
+
+        } catch (SQLException e) {
+            log.info("db error", e);
+            throw e;
+        } finally {
+            // 커넥션은 유지해야 되기 때문에 닫지 않는다.
+            JdbcUtils.closeResultSet(rs);
+            JdbcUtils.closeStatement(pstmt);
+        }
+    }
+
+
+
     public void update(String memberId, int money) throws SQLException {
         String sql = "update member set money = ? where member_id = ?";
 
@@ -96,6 +129,28 @@ public class MemberRepositoryV1 {
             close(con, pstmt, rs);
         }
     }
+
+    public void update(Connection con, String memberId, int money) throws SQLException {
+        String sql = "update member set money = ? where member_id = ?";
+
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+            int count = pstmt.executeUpdate();
+            log.info("update count = {}", count);
+        } catch (SQLException e) {
+            log.info("db error", e);
+            throw e;
+        } finally {
+            JdbcUtils.closeResultSet(rs);
+            JdbcUtils.closeStatement(pstmt);
+        }
+    }
+
     public void delete(String memberId) throws SQLException {
         String sql = "delete from member where member_id = ?";
 
---
+++ src/main/java/hello/jdbc/service/MemberServiceV2.java
@@ -0,0 +1,53 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.support.JdbcUtils;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 서비스
+ */
+@RequiredArgsConstructor
+public class MemberServiceV2 {
+    private final MemberRepositoryV2 memberRepositoryV2;
+    private final DataSource dataSource;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Connection con = dataSource.getConnection();
+        try {
+            con.setAutoCommit(false);
+
+            bizLogic(con, fromId, toId, money);
+
+            con.commit();
+        } catch (Exception e) {
+            con.rollback();
+            throw new IllegalStateException(e);
+        } finally {
+            con.setAutoCommit(true);
+            JdbcUtils.closeConnection(con);
+        }
+
+    }
+
+    private void bizLogic(Connection con, String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepositoryV2.findById(con, fromId);
+        Member toMember = memberRepositoryV2.findById(con, toId);
+
+        memberRepositoryV2.update(con, fromId, fromMember.getMoney() - money);
+        validation(toMember);
+
+        memberRepositoryV2.update(con, toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
